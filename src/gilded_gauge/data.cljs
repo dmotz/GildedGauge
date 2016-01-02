13dc5e5ae1c779bf716429502d2400ddf2ef8efb
Dan Motzenbecker <dan@oxism.com>
2016-01-01 22:23:25 -0500

    basic data

---
+++ src/gilded_gauge/data.cljs
@@ -0,0 +1,34 @@
+(ns gilded-gauge.data)
+
+
+;; https://en.wikipedia.org/wiki/The_World%27s_Billionaires_2015
+
+(def ranked
+  (partition
+   2
+   ["Bill Gates"          79.2
+    "Carlos Slim"         77.1
+    "Warren Buffett"      72.7
+    "Amancio Ortega"      64.5
+    "Larry Ellison"       54.3
+    "Charles Koch"        42.9
+    "David Koch"          42.9
+    "Christy Walton"      41.7
+    "Jim Walton"          40.6
+    "Liliane Bettencourt" 40.1]))
+
+(def images
+  {"Bill Gates"     "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Bill_Gates_June_2015.jpg/440px-Bill_Gates_June_2015.jpg"
+   "Carlos Slim"    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Carlos_Slim_Hel%C3%BA.jpg/440px-Carlos_Slim_Hel%C3%BA.jpg"
+   "Warren Buffett" "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Warren_Buffett_KU_Visit.jpg/440px-Warren_Buffett_KU_Visit.jpg"
+   "Larry Ellison"  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Larry_Elllison_on_stage.jpg/440px-Larry_Elllison_on_stage.jpg"
+   "David Koch"     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/David_Koch_by_Gage_Skidmore.jpg/440px-David_Koch_by_Gage_Skidmore.jpg"
+   "Jim Walton"     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Jim_Walton_attends_shareholders_meeting.jpg/440px-Jim_Walton_attends_shareholders_meeting.jpg"})
+
+
+(def median-us-income 53657)
+(def median-global-income 9733)
+
+(def stats
+  {"years of median U.S. household income"   median-us-income
+   "years of median global household income" median-global-income})
