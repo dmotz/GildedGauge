bfe898074c43eba9750ef3eb5a4da3576a1e6ba5
Dan Motzenbecker <dan@oxism.com>
2016-01-02 18:38:06 -0500

    added timeline layout

--- src/gilded_gauge/core.cljs
+++ src/gilded_gauge/core.cljs
@@ -21,58 +21,62 @@
                equiv       (calc-equiv rich-worth net-worth amount)]
           (html
             [:div
-              [:h1 {:id "logo"} "Gilded Gauge"]
+              [:h1#logo "Gilded Gauge"]
 
-              [:div.column
-                [:span "If I have a net worth of "]
-                [:div.input-holder
-                  [:span.input-wrap
-                    (input :net-worth net-worth)]
-                  [:div.range-slider (preset-list presets/worths net-worth)]]
+              [:div#columns
+                [:div.column
+                  [:span "If I have a net worth of "]
+                  [:div.input-holder
+                    [:span.input-wrap
+                      (input :net-worth net-worth)]
+                    [:div.range-slider (preset-list presets/worths :net-worth)]]
 
-                [:span ", then me spending "]
+                  [:span ", then me spending "]
 
-                [:div.input-holder
-                  [:span.input-wrap
-                    (input :amount amount)]
-                  [:div.range-slider
-                    [:input
-                      {:type      "range"
-                       :value     amount
-                       :on-change #(update-num :amount %)
-                       :min       0
-                       :max       net-worth
-                       :step      "10"}]
-                    (preset-list presets/amounts amount)]]
+                  [:div.input-holder
+                    [:span.input-wrap
+                      (input :amount amount)]
+                    [:div.range-slider
+                      [:input
+                        {:type      "range"
+                         :value     amount
+                         :on-change #(update-num :amount %)
+                         :min       0
+                         :max       net-worth
+                         :step      "10"}]
+                      (preset-list presets/amounts :amount)]]
 
-                [:span "is the equivalent of…"]]
+                  [:span " is the equivalent of…"]]
 
-              [:div.column
-                (let [src (get data/images rich-name)]
-                  [:div#portrait
-                    {:style    (when src {:backgroundImage (str "url(" src ")")})
-                     :on-click toggle-person-select}
-                    (when-not src [:div (get-initials rich-name)])])
+                [:div.column
+                  (let [src (get data/images rich-name)]
+                    [:div#portrait
+                      {:style    (when src {:backgroundImage (str "url(" src ")")})
+                       :on-click toggle-person-select}
+                      (when-not src [:div (get-initials rich-name)])])
 
-                [:span#current-person
-                  {:on-click toggle-person-select}
-                  (first (nth data/ranked current-person))]
+                  [:span#current-person
+                    {:on-click toggle-person-select}
+                    (first (nth data/ranked current-person))]
 
-                (when show-person-select
-                  [:ul#person-list
-                    (map-indexed
-                      (fn [i [name]]
-                        (when (not= i current-person)
-                          [:li {:key i :on-click #(select-person i)} name]))
-                      data/ranked)])
+                  (when show-person-select
+                    [:ul#person-list
+                      (map-indexed
+                        (fn [i [name]]
+                          (when (not= i current-person)
+                            [:li {:key i :on-click #(select-person i)} name]))
+                        data/ranked)])
 
-                [:span " spending "]
-                [:span.amount (str "$" (format-number equiv))]
-                [:span "."]
-                [:br]
-                [:br]
-                [:span "Put another way:"]
-                (stats equiv net-worth)]])))))
+                  [:span " spending "]
+                  [:span.amount (str "$" (format-number equiv))]
+                  [:span "."]
+                  [:br]
+                  [:br]
+                  [:span "Put another way:"]
+                  (stats equiv net-worth)]]
+
+              [:div#timeline
+                [:div#baseline]]])))))
 
   app
   {:target (. js/document (getElementById "app"))})
