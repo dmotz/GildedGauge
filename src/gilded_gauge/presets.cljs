bcc380a15d1dbaade288d119046a0518a1a6bbe2
Dan Motzenbecker <dan@oxism.com>
2016-01-21 17:53:25 -0500

    more worth presets

--- src/gilded_gauge/presets.cljs
+++ src/gilded_gauge/presets.cljs
@@ -13,8 +13,10 @@
 
 
 (def worths
-  {"median U.S. household income"   data/median-us-income
-   "median global household income" data/median-global-income})
+  (merge
+    {"median U.S. household income"   data/median-us-income
+     "median global household income" data/median-global-income}
+    ($zip [1e4 5e4 1e5 25e4 5e5 1e6 25e5 5e6])))
 
 
 (def net-worth-min 5)
