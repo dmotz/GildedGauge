c92cf46631def830c59b63560c1f4e4fffc07863
Dan Motzenbecker <dan@oxism.com>
2016-03-15 20:52:17 -0400

    swapped preset map entries

--- src/gilded_gauge/presets.cljs
+++ src/gilded_gauge/presets.cljs
@@ -6,19 +6,19 @@
 (def $format (comp (partial str \$) format-number))
 
 (defn $zip [xs]
-  (into (sorted-map) (zipmap xs (map $format xs))))
+  (zipmap xs (map $format xs)))
 
 
-(def amounts ($zip [1 5 10 20 50 100 500 1000 5000]))
+(def amounts (into (sorted-map) ($zip [1 5 10 20 50 100 500 1000 5000])))
 
 
 (def worths
-  (sort-by
-    second
+  (into
+    (sorted-map)
     (merge
-      {"average U.S. net worth"         45000
-       "median U.S. household income"   data/median-us-income
-       "median global household income" data/median-global-income}
+      {45000                     "average U.S. net worth"
+       data/median-us-income     "median U.S. household income"
+       data/median-global-income "median global household income"}
       ($zip [1e4 5e4 1e5 25e4 5e5 1e6 25e5 5e6]))))
 
 
