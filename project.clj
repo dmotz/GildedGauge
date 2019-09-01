08263acb2e02b8ae08aa5c344d50bcc385c8b424
Dan Motzenbecker <dan@oxism.com>
2019-09-01 14:29:26 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,20 +1,18 @@
 (defproject gilded-gauge "0.1.0-SNAPSHOT"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [org.clojure/core.async "0.2.391"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [org.clojure/core.async "0.4.500"]
                  [sablono "0.7.2"]
                  [org.omcljs/om "0.9.0"]
                  [cljsjs/react-dom "15.2.1-1"]
-                 [cljsjs/react-dom-server "15.2.1-1"]
                  [cljsjs/pixi "3.0.10-0"]
                  [cljsjs/matter "0.9.1-0"]
                  [org.clojure/data.json "0.2.6"]
-                 [enlive "1.1.6"]
-                 [proto-repl "0.3.1"]]
+                 [enlive "1.1.6"]]
 
-  :plugins [[lein-cljsbuild "1.1.4"]
-            [lein-figwheel "0.5.7"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.19"]]
 
   :source-paths ["src"]
 
