9536e65e6f63f4c49c7c0b0a966ff9d813cb03ca
Dan Motzenbecker <dan@oxism.com>
2020-11-05 18:21:38 -0500

    update ranking

--- src/gilded_gauge/rankings.cljs
+++ src/gilded_gauge/rankings.cljs
@@ -4,110 +4,110 @@
 (def
  rankings
  [["Jeff Bezos"
-   191.0
+   195.0
    "thumb/6/6c/Jeff_Bezos_at_Amazon_Spheres_Grand_Opening_in_Seattle_-_2018_%2839074799225%29_%28cropped%29.jpg/220px-Jeff_Bezos_at_Amazon_Spheres_Grand_Opening_in_Seattle_-_2018_%2839074799225%29_%28cropped%29.jpg"]
   ["Bill Gates"
-   124.0
+   126.0
    "thumb/a/a0/Bill_Gates_2018.jpg/220px-Bill_Gates_2018.jpg"]
   ["Mark Zuckerberg"
-   109.0
+   111.0
    "thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"]
   ["Elon Musk"
-   105.0
+   109.0
    "thumb/e/ed/Elon_Musk_Royal_Society.jpg/220px-Elon_Musk_Royal_Society.jpg"]
   ["Bernard Arnault"
-   90.9
+   95.9
    "thumb/d/de/Bernard_Arnault_%283%29_-_2017_%28cropped%29.jpg/220px-Bernard_Arnault_%283%29_-_2017_%28cropped%29.jpg"]
   ["Larry Page"
-   81.8
+   82.5
    "thumb/e/ec/Larry_Page_in_the_European_Parliament%2C_17.06.2009_%28cropped%29.jpg/220px-Larry_Page_in_the_European_Parliament%2C_17.06.2009_%28cropped%29.jpg"]
-  ["Sergey Brin"
-   79.2
-   "thumb/a/a2/Sergey_Brin_cropped.jpg/220px-Sergey_Brin_cropped.jpg"]
   ["Steve Ballmer"
-   78.1
+   80.5
    "thumb/4/44/Steve_Ballmer_2014.jpg/220px-Steve_Ballmer_2014.jpg"]
+  ["Sergey Brin"
+   79.9
+   "thumb/a/a2/Sergey_Brin_cropped.jpg/220px-Sergey_Brin_cropped.jpg"]
   ["Warren Buffett"
-   78.1
+   78.6
    "thumb/5/51/Warren_Buffett_KU_Visit.jpg/220px-Warren_Buffett_KU_Visit.jpg"]
   ["Mukesh Ambani"
-   72.7
+   74.5
    "thumb/6/69/Mukesh_Ambani.jpg/220px-Mukesh_Ambani.jpg"]
-  ["Francoise Bettencourt Meyers" 70.2]
+  ["Francoise Bettencourt Meyers" 70.6]
   ["Larry Ellison"
-   67.8
+   68.4
    "thumb/0/00/Larry_Ellison_picture.png/220px-Larry_Ellison_picture.png"]
-  ["MacKenzie Scott" 62.3]
-  ["Rob Walton" 61.3]
+  ["MacKenzie Scott" 63.9]
+  ["Pony Ma" 61.9 "thumb/d/d9/Pony_Ma_2011.jpg/220px-Pony_Ma_2011.jpg"]
+  ["Rob Walton" 61.9]
   ["Jim Walton"
-   61.1
+   61.7
    "thumb/b/bb/Jim_Walton_attends_shareholders_meeting.jpg/220px-Jim_Walton_attends_shareholders_meeting.jpg"]
   ["Alice Walton"
-   61.1
+   61.7
    "thumb/e/e1/Alice_Walton_%28cropped%29.jpg/220px-Alice_Walton_%28cropped%29.jpg"]
-  ["Pony Ma" 58.5 "thumb/d/d9/Pony_Ma_2011.jpg/220px-Pony_Ma_2011.jpg"]
-  ["Jack Ma"
-   57.4
-   "thumb/a/a9/Enabling_eCommerce-_Small_Enterprises%2C_Global_Players_%2839008130265%29_%28cropped%29.jpg/220px-Enabling_eCommerce-_Small_Enterprises%2C_Global_Players_%2839008130265%29_%28cropped%29.jpg"]
-  ["Zhong Shanshan" 56.9]
   ["Amancio Ortega"
-   55.8
+   57.7
    "thumb/5/59/Amancio_Ortega_Portrait_Painting_Collage_By_Danor_Shtruzman.jpg/220px-Amancio_Ortega_Portrait_Painting_Collage_By_Danor_Shtruzman.jpg"]
-  ["Julia Flesher Koch" 55.6]
+  ["Zhong Shanshan" 57.1]
+  ["Jack Ma"
+   56.6
+   "thumb/a/a9/Enabling_eCommerce-_Small_Enterprises%2C_Global_Players_%2839008130265%29_%28cropped%29.jpg/220px-Enabling_eCommerce-_Small_Enterprises%2C_Global_Players_%2839008130265%29_%28cropped%29.jpg"]
+  ["Julia Flesher Koch" 56.6]
   ["Charles Koch"
-   55.6
+   56.6
    "thumb/2/20/Charles_Koch_portrait_%28cropped%29.jpg/220px-Charles_Koch_portrait_%28cropped%29.jpg"]
   ["Michael Bloomberg"
    54.9
    "thumb/e/e2/Mike_Bloomberg_Headshot.jpg/220px-Mike_Bloomberg_Headshot.jpg"]
   ["Carlos Slim"
-   45.9
+   47.7
    "thumb/b/bf/Carlos_Slim_%2845680472234%29_%28cropped%29.jpg/220px-Carlos_Slim_%2845680472234%29_%28cropped%29.jpg"]
   ["Phil Knight"
-   43.4
+   44.2
    "thumb/b/bc/Philknightfootball.jpg/200px-Philknightfootball.jpg"]
   ["Francois Pinault"
-   42.4
+   43.7
    "thumb/4/48/Fran%C3%A7ois_Pinault_Stade_rennais_-_Le_Havre_AC_20150708_44.jpg/220px-Fran%C3%A7ois_Pinault_Stade_rennais_-_Le_Havre_AC_20150708_44.jpg"]
+  ["John Mars" 41.2]
   ["Jacqueline Mars"
-   41.1
+   41.2
    "thumb/5/5c/Jacqueline_Mars_Portrait_Painting_Collage_By_Danor_Shtruzman.jpg/220px-Jacqueline_Mars_Portrait_Painting_Collage_By_Danor_Shtruzman.jpg"]
-  ["John Mars" 41.0]
-  ["Colin Huang" 38.9]
+  ["Colin Huang" 39.8]
   ["Tadashi Yanai"
-   36.2
+   36.5
    "thumb/1/10/Tadashi_Yanai.jpg/220px-Tadashi_Yanai.jpg"]
   ["Michael Dell"
-   35.3
+   36.2
    "thumb/c/ce/Michael_Dell_2010.jpg/220px-Michael_Dell_2010.jpg"]
-  ["Gerard Wertheimer" 34.1]
-  ["Alain Wertheimer" 34.1]
+  ["Gerard Wertheimer" 34.8]
+  ["Alain Wertheimer" 34.8]
   ["Dan Gilbert"
-   33.9
+   34.3
    "thumb/b/b6/Dan_Gilbert_Chairman_of_Quicken_Loans_and_Majority_Owner_of_the_Cleveland_Cavaliers.jpg/220px-Dan_Gilbert_Chairman_of_Quicken_Loans_and_Majority_Owner_of_the_Cleveland_Cavaliers.jpg"]
-  ["He Xiangjian" 32.8]
-  ["Giovanni Ferrero" 32.3]
+  ["He Xiangjian" 33.3]
   ["Li Ka-Shing"
-   32.2
+   33.3
    "thumb/e/e8/Li_Ka_Shing.jpg/220px-Li_Ka_Shing.jpg"]
   ["Laurene Powell Jobs"
-   32.2
+   33.0
    "thumb/9/97/Laurene_Powell_Jobs.jpg/220px-Laurene_Powell_Jobs.jpg"]
+  ["Giovanni Ferrero" 32.6]
   ["Len Blavatnik"
-   31.6
+   31.7
    "thumb/e/ea/Leonard_Blavatnik%2C_February_2018_%284568%29_%28cropped%29.jpg/220px-Leonard_Blavatnik%2C_February_2018_%284568%29_%28cropped%29.jpg"]
-  ["Abby  Johnson" 30.7]
   ["Sheldon Adelson"
-   30.1
+   31.3
    "thumb/f/f9/Sheldon_Adelson_crop.jpg/220px-Sheldon_Adelson_crop.jpg"]
-  ["William Ding" 29.7]
-  ["Yang Huiyan" 28.0]
-  ["Takemitsu Takizaki" 27.7]
-  ["Vladimir Potanin" 25.6 "7/7d/Vladimir_Potanin.jpg"]
+  ["Abby  Johnson" 31.3]
+  ["Yang Huiyan" 29.6]
+  ["William Ding" 29.1]
+  ["Takemitsu Takizaki" 28.9]
   ["Zhang Zhidong"
-   25.2
+   26.8
    "thumb/5/50/%E5%BC%B5%E4%B9%8B%E6%B4%9E%E6%9C%9D%E6%9C%8D%E7%85%A7.jpg/200px-%E5%BC%B5%E4%B9%8B%E6%B4%9E%E6%9C%9D%E6%9C%8D%E7%85%A7.jpg"]
-  ["Wang Wei" 24.9]
-  ["Pang Kang" 24.7]
-  ["Leonard  Lauder" 24.6]
-  ["Hui Ka Yan" 24.5]])
+  ["Vladimir Potanin" 26.0 "7/7d/Vladimir_Potanin.jpg"]
+  ["Pang Kang" 25.9]
+  ["Leonard  Lauder" 25.4]
+  ["Hui Ka Yan" 25.2]
+  ["Goh Cheng Liang" 25.2]])
